a018602995a3fa4adc4466343318c98f74686775
leeseuldist <seuldist@gmail.com>
2025-07-02 11:16:10 +0900

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -5,15 +5,19 @@ export default async function handler(req, res) {
     const scriptURL = "https://script.google.com/macros/s/AKfycbxh_1oPMtn113PZiYmdvFxYiOzt46tFRQQyd9EH_ES9xtdVtBlAwL49YIoaBG1Jz1OI/exec";
 
     try {
-      await fetch(scriptURL, {
+      const response = await fetch(scriptURL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, company, phone, message }),
       });
 
+      const text = await response.text();
+      console.log("Google Script 응답:", text);
+
       return res.status(200).json({ message: "success" });
     } catch (error) {
-      return res.status(500).json({ message: "error", error });
+      console.error("오류 발생:", error);  // 🔥 로그 찍기
+      return res.status(500).json({ message: "error", error: error.message });
     }
   } else {
     res.status(405).json({ message: "Method not allowed" });
